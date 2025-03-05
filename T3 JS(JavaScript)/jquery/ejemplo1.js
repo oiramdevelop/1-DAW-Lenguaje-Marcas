63b45b3334aac43312d25a8569b6676da44de312
Mario <oiramdeveloper@gmail.com>
2025-03-05 08:03:58 +0100

    actu

--- T3 JS(JavaScript)/jquery/ejemplo1.js
+++ T3 JS(JavaScript)/jquery/ejemplo1.js
@@ -1,2 +1,34 @@
 $(document).ready(main)
 
+function mostrar_pares(){
+    let objetos = $(".p_par")
+    objetos.slideDown()
+}
+
+function mostrar_impares(){
+    let objetos = $(".p_impar")
+    objetos.slideDown()
+}
+
+function ocultar_pares(){
+    let objetos = $(".p_par")
+    objetos.slideUp()
+}
+
+function ocultar_impares(){
+    let objetos = $(".p_impar")
+    objetos.slideUp()
+}
+
+
+function main(){
+
+    $("#mostrar_pares").click(mostrar_pares)
+    $("#mostrar_impares").click(mostrar_impares)
+
+
+    
+    $("#ocultar_pares").click(ocultar_pares)
+    $("#ocultar_impares").click(ocultar_impares)
+
+}
