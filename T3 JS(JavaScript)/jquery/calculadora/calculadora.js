02de94033146cd02eb49357003ecf82ae4c3dce2
Mario <oiramdeveloper@gmail.com>
2025-03-11 14:23:33 +0100

    actu

--- T3 JS(JavaScript)/jquery/calculadora/calculadora.js
+++ T3 JS(JavaScript)/jquery/calculadora/calculadora.js
@@ -1,9 +1,54 @@
-$(document).ready(function(e){
-    $(".calcular").click(function(e){
-        var , altura, resultado;
-        base=Number($(".base").val());
-        altura=Number($(".altura").val());
-        resultado=1/2*base*altura;
-        $(".resultado").html("<p>El area del triangulo es "+resultado+"</p>");
+$(document).ready(function() {
+    let currentInput = '';
+    let operator = '';
+    let firstOperand = '';
+    let secondOperand = '';
+
+    $('button').on('click', function() {
+        const value = $(this).data('value');
+
+        if (value === 'C') {
+            currentInput = '';
+            operator = '';
+            firstOperand = '';
+            secondOperand = '';
+            $('#display').val('');
+        } else if (value === '=') {
+            if (firstOperand && operator && currentInput) {
+                secondOperand = currentInput;
+                const result = calculate(firstOperand, secondOperand, operator);
+                $('#display').val(result);
+                currentInput = result;
+                operator = '';
+                firstOperand = '';
+                secondOperand = '';
+            }
+        } else if (['+', '-', '*', '/'].includes(value)) {
+            if (currentInput) {
+                firstOperand = currentInput;
+                operator = value;
+                currentInput = '';
+            }
+        } else {
+            currentInput += value;
+            $('#display').val(currentInput);
+        }
     });
+
+    function calculate(a, b, op) {
+        a = parseFloat(a);
+        b = parseFloat(b);
+        switch (op) {
+            case '+':
+                return a + b;
+            case '-':
+                return a - b;
+            case '*':
+                return a * b;
+            case '/':
+                return a / b;
+            default:
+                return 0;
+        }
+    }
 });
